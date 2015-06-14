2ddc6f6978743d6acab15496c1ff3ff9a75ff291
chfw <wangc_2011@hotmail.com>
2015-06-14 19:08:25 +0100

    remove unwanted dependency

--- myproject/__init__.py
+++ myproject/__init__.py
@@ -5,7 +5,6 @@ def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
     config = Configurator(settings=settings)
-    config.include('pyramid_chameleon')
     config.include('pyramid_excel')
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
