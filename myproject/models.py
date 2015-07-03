3566ee14aa072e952f156711d4c9db79db74ad43
chfw <wangc_2011@hotmail.com>
2015-07-03 18:52:22 +0100

    Oh... I missed a file, my bad

---
+++ myproject/models.py
@@ -0,0 +1,59 @@
+from sqlalchemy import (
+    Column,
+    Index,
+    Integer,
+    Text,
+    String,
+    ForeignKey,
+    DateTime,
+    create_engine
+    )
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, backref
+from sqlalchemy.orm import (
+    scoped_session,
+    sessionmaker,
+    )
+
+from zope.sqlalchemy import ZopeTransactionExtension
+import datetime
+
+DBSession = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
+Base = declarative_base()
+
+
+class Post(Base):
+    __tablename__ = 'post'
+    id = Column(Integer, primary_key=True)
+    title = Column(String(80))
+    body = Column(Text)
+    pub_date = Column(DateTime)
+
+    category_id = Column(Integer, ForeignKey('category.id'))
+    category = relationship('Category',
+        backref=backref('posts', lazy='dynamic'))
+
+    def __init__(self, title, body, category, pub_date=None):
+        self.title = title
+        self.body = body
+        if pub_date is None:
+            pub_date = datetime.utcnow()
+        self.pub_date = pub_date
+        self.category = category
+
+    def __repr__(self):
+        return '<Post %r>' % self.title
+
+
+class Category(Base):
+    __tablename__ = 'category'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50))
+
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return '<Category %r>' % self.name
+
