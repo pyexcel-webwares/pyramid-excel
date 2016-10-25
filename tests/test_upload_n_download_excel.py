15bc2d6b5b2bef8d1afa103d98c64383e059d296
chfw <wangc_2011@hotmail.com>
2016-10-25 09:28:20 +0100

    update test cases

--- tests/test_upload_n_download_excel.py
+++ tests/test_upload_n_download_excel.py
@@ -39,11 +39,11 @@ class TestExcelResponse:
                 io = sheet.save_to_memory(upload_file_type)
                 if not PY2:
                     if isinstance(io, BytesIO):
-                        content = io.getvalue()
+                        content = io
                     else:
-                        content = io.getvalue().encode('utf-8')
+                        content = io.encode('utf-8')
                 else:
-                    content = io.getvalue()
+                    content = io
                 response = self.app.post(
                     '/switch/%s' % download_file_type,
                     upload_files=[('file', file_name, content)],
