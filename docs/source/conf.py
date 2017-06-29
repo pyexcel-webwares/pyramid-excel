44e0fe8bb5e3f0967549dc876917904d1d6200c5
chfw <wangc_2011@hotmail.com>
2017-06-29 14:31:57 +0100

    update version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,7 +21,7 @@ master_doc = 'index'
 
 project = u'pyramid-excel'
 copyright = u'2015-2017 Onni Software Ltd.'
-version = '0.0.3'
+version = '0.0.4'
 release = '0.0.4'
 exclude_patterns = []
 pygments_style = 'sphinx'
