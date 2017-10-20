226029afb82f1459b44753306c1050be6e86389c
chfw <wangc_2011@hotmail.com>
2017-10-20 23:58:55 +0100

    :handshake: take latest code from setupmobans, carrying LICENSE file, https://github.com/pyexcel/pyexcel/pull/103, update docs https://github.com/pyexcel/pyexcel/pull/104

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
 pip freeze
-nosetests --with-cov --cover-package pyramid_excel --cover-package tests --with-doctest --doctest-extension=.rst README.rst tests docs/source pyramid_excel && flake8 . --exclude=.moban.d --builtins=unicode,xrange,long
+nosetests --with-coverage --cover-package pyramid_excel --cover-package tests --with-doctest --doctest-extension=.rst README.rst tests docs/source pyramid_excel && flake8 . --exclude=.moban.d --builtins=unicode,xrange,long
