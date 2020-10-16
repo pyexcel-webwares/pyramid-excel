0eb8df6eccecb3ddee50ddf092031f26aa7a4f55
chfw <wangc_2011@hotmail.com>
2020-10-16 22:41:39 +0100

    :handshake: update project meta data

--- setup.py
+++ setup.py
@@ -94,13 +94,14 @@ EXTRAS_REQUIRE = {
 }
 # You do not need to read beyond this line
 PUBLISH_COMMAND = "{0} setup.py sdist bdist_wheel upload -r pypi".format(sys.executable)
+HERE = os.path.abspath(os.path.dirname(__file__))
+
 GS_COMMAND = ("gs pyramid-excel v0.0.5 " +
               "Find 0.0.5 in changelog for more details")
 NO_GS_MESSAGE = ("Automatic github release is disabled. " +
                  "Please install gease to enable it.")
 UPLOAD_FAILED_MSG = (
     'Upload failed. please run "%s" yourself.' % PUBLISH_COMMAND)
-HERE = os.path.abspath(os.path.dirname(__file__))
 
 
 class PublishCommand(Command):
@@ -145,7 +146,6 @@ class PublishCommand(Command):
 SETUP_COMMANDS.update({
     "publish": PublishCommand
 })
-
 
 def has_gease():
     """
